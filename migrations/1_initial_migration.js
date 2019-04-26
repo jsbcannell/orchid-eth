3a9753134e496efabea1e7a81329b98eb9a7fe03
jake cannell <jake.cannell@gmail.com>
2019-04-26 13:47:35 -0700

    fix for bitcore-lib multi dep issue

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
+
+    // fix for "More than one instance of bitcore-lib found. Please make sure to require bitcore-lib and check that submodules do not also include their own bitcore-lib dependency"
+    // https://github.com/bitpay/bitcore/issues/1457#issuecomment-467594031
+    Object.defineProperty(global, '_bitcore', { get(){ return undefined }, set(){} });
+
   deployer.deploy(Migrations);
 };
